3a88129b437c40bf2d2daaafa0353a77cbbc7d65
Traves Theberge <traves.theberge@gmail.com>
2024-06-27 20:22:01 -0400

    updates

--- tina/config.ts
+++ tina/config.ts
@@ -5,15 +5,15 @@ const branch =
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "main";
+  "master";
 
 export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.TINA_CLIENT_ID || "",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: process.env.TINA_TOKEN || "",
 
   build: {
     outputFolder: "admin",
